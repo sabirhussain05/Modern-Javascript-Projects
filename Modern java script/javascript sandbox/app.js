fbeac278b7a3f7fd32822539216c94dca19c23be
Sabir Hussain <42651006+sabirshafique@users.noreply.github.com>
2021-03-12 10:56:23 +0500

    Add files via upload

---
+++ Modern java script/javascript sandbox/app.js
@@ -0,0 +1,97 @@
+
+// var name =' name 1:sabir';
+// console.log(name);
+// console.warn('stay happy');
+// // reassignment of variable
+// name = " name 2:sabir hussain";
+// console.log(name);
+// const person ={
+//   name: 'sabir',
+//   class: '9th',
+
+
+
+
+// }
+// person.name='sabir hussain';
+// person.class=32;
+// console.log(person);
+
+// const numbers=[1,2,3,4,5];
+// numbers.push(6);
+// console.log(numbers);
+
+// permitive
+
+// string
+// const numbers ='sabir';
+// console.log(typeof numbers);
+// // num
+// const age=22;
+// console.log(typeof age);
+// // boolean
+// const x=true;
+// console.log(typeof x);
+// // null
+// const x2=null;
+// console.log(typeof x2);
+
+// // !reference type is obeject
+// const hobbies=['ali','hassam'];
+// console.log( typeof hobbies);
+// const today=new Date();
+// console.log(today);
+
+// let val;
+// val = String(222);
+// val =String(true);
+// val=String(new Date());
+// val= String([1,2,3,4,5]);
+// val=(5).toString();
+
+
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// const num1=100;
+// const num2=120;
+// let val;
+// val = num1+num2;
+// val = num1-num2;
+// val = num1*num2;
+// val = num1%num2;
+
+// val=Math.PI;
+// val =Math.random();
+// Math.random()*20;
+// console.log(val);
+// const firtsname =" sabir";
+// const secondname ="hussain";
+// let val;
+// val = firtsname+ ' '+secondname;
+// val='sabir';
+// val+=' hussain';
+
+// console.log(val);
+
+
+// if then else statements
+// const name='sabir';
+// const age=20;
+// if (name>=19 && name<=20){
+//   console.log(`${name} is a teeneager`);
+// }
+// else 
+// {
+// console.log(`${name} is an adult`)
+// }
+// window output operations
+// const input = prompt();
+// alert(input);
+
+// are u sure?
+if(confirm('are u sure?')){
+  console.log('yes');
+}else {
+  console.log('no');
+}
